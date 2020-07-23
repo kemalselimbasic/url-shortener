faae4e4d9ef2a36d43a0585f715826e6ecf77735
ksprivate <kemal.selimbasic.2016@gmail.com>
2020-07-23 04:22:54 +0200

    fixed https error

--- resources/views/done.blade.php
+++ resources/views/done.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <h1>URL</h1><h2>{{$url}}</h2> <br>
         <h1>SLUG</h1><h2>{{$slug}}</h2><br>
-        <h1>LINK</h1><h2> <a href="https://127.0.0.1:8000/{{$slug}}">https://127.0.0.1:8000/{{$slug}}</a></h2>
+        <h1>LINK</h1><h2> <a href="http://127.0.0.1:8000/{{$slug}}">http://127.0.0.1:8000/{{$slug}}</a></h2>
 
     </div>
 @endsection
